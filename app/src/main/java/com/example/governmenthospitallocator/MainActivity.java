20c05361901bda9492a10edc774fa80cbb271e43
ZayeemMohd <zayeemudin123@gmail.com>
2023-12-12 12:35:48 +0530

    landing activity and UI templete

---
+++ app/src/main/java/com/example/governmenthospitallocator/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.governmenthospitallocator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    Timer timer; //adding timer class to auto switch activity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //declaring timer
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },2000);
+    }
+}
